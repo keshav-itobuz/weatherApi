16c9db4745db6e34381ba366668e996d77ae1dd5
keshav-itobuz <keshav@itobuz.com>
2024-03-12 15:08:12 +0530

    updated

--- app.js
+++ app.js
@@ -7,39 +7,36 @@ app.use(cors())
 
 app.use(express.json()) // its a middleware 
 
-const dbData = fs.readFileSync('./data.json' , 'utf-8');
-const output = {
-    data:"",
-    status:"",
-    message:"",
-};
-
+const dbData = fs.readFileSync('./data.json', 'utf-8');
 const data = JSON.parse(dbData);
+const output = {
+    data: "",
+    status: "",
+    message: "",
+};
+const port = 5500;
 
-
-app.get('/api/v1/weather',(req , res)=>{
-    let city = req.query.location;
-    const userAskedData = data.find((element)=>element.name.toLowerCase()===city.toLowerCase())
-    if(userAskedData){
-        output.data=userAskedData;
-        output.status="200 Ok";
-        output.message="Success"
+app.get('/api/v1/weather', (req, res) => {
+    const city = req.query.location;
+    const userAskedData = data.find((element) => element.name.toLowerCase() === city.toLowerCase())
+    if (userAskedData) {
+        output.data = userAskedData;
+        output.status = "200 Ok";
+        output.message = "Success"
     }
-
-    else{
-        output.data="NULL";
-        output.status="404 Error";
-        output.message="Fail"
+    else {
+        output.data = "NULL";
+        output.status = "404 Error";
+        output.message = "Fail"
     }
     res.json(output);
     res.end();
 });
 
-app.get('*',(req , res)=>{
+app.get('*', (req, res) => {
     res.send("Wrong url");
 });
 
-const port = 5500;
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 })
